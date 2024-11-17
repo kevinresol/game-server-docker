c446c8ee82a1e9ba9905f4d8a47c4142c97926a2
Kevin Leung <kevinresol@gmail.com>
2024-11-17 13:41:03 +1100

    feat: use a base image

--- games/project-zomboid/entrypoint.sh
+++ games/project-zomboid/entrypoint.sh
@@ -2,10 +2,8 @@
 
 set -ex
 
-# printenv
-# ls -lah /home/steam/Zomboid
-
+# modify server config file
 node /home/steam/tool.js config --env-var-prefix=PZ_
 
-
+# start the server
 "$GAME_BIN_PATH/start-server.sh" -cachedir="$GAME_DATA_PATH" -servername "$SERVER_NAME" "$@"
