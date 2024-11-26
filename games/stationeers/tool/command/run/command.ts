45a63375fd5d0aa382b1048ba44047873107de4f
Kevin Leung <kevinresol@gmail.com>
2024-11-26 13:06:03 +1100

    feat(stationeers): handle logging

--- games/stationeers/tool/command/run/command.ts
+++ games/stationeers/tool/command/run/command.ts
@@ -1,10 +1,11 @@
-import handler from "./handler";
+import { option as o } from "@why-ts/cli";
 import baseCommand from "../common";
+import handler from "./handler";
 
-export type BaseArgs = {
-	dataPath: string;
-	serverName: string;
-};
+// export type BaseArgs = {
+// 	dataPath: string;
+// 	serverName: string;
+// };
 
 export default baseCommand
 	.meta({
@@ -12,4 +13,11 @@ export default baseCommand
 			'Run the game. Any arguments after "--" will be forwarded to the server executable. ' +
 			"This will also intercept SIGINT and perform autosave before quitting.",
 	})
+	.option(
+		"log-file",
+		o.string({
+			description: "Path to the game log file.",
+			fallback: () => process.env.GAME_LOG_FILE,
+		})
+	)
 	.handle(handler);
--- games/stationeers/tool/command/run/handler.ts
+++ games/stationeers/tool/command/run/handler.ts
@@ -1,14 +1,29 @@
 import { HandlerInput } from "@why-ts/cli";
 import { spawn } from "node:child_process";
+import { match } from "ts-pattern";
 
 type Args = {
 	binPath: string;
+	logFile?: string;
 };
-export default function ({ args }: HandlerInput<Args>) {
-	const proc = spawn(
-		`${args.binPath}/rocketstation_DedicatedServer.x86_64`,
-		args["--"]
-	);
+export default function ({
+	args: { binPath, logFile, "--": args },
+}: HandlerInput<Args>) {
+	if (logFile) {
+		match(args.findIndex((arg) => arg.toLowerCase() === "-logfile"))
+			.with(-1, () => args.push("-logfile", logFile))
+			.otherwise(() => {
+				throw new Error(
+					"Do not specify -logfile manually in the rest arguments, use the --log-file flag or GAME_LOG_FILE env var instead."
+				);
+			});
+	}
+
+	const proc = spawn(`${binPath}/rocketstation_DedicatedServer.x86_64`, args);
+
+	// pipe the server process output to the console
+	proc.stdout.pipe(process.stdout);
+	proc.stderr.pipe(process.stderr);
 
 	// let quitting = false;
 	// Intercept SIGINT/SIGTERM and send `autosavecancel` command to the server process
@@ -31,12 +46,27 @@ export default function ({ args }: HandlerInput<Args>) {
 	// process.on("SIGINT", makdSignalHandler("SIGINT"));
 	process.on("SIGTERM", () => proc.kill("SIGINT")); // forwards SIGTERM to the server process as SIGINT as Unity only hanldes SIGINT
 
-	proc.stdout.pipe(process.stdout);
-	proc.stderr.pipe(process.stderr);
+	// tail the log file if specified
+	const log = match(logFile)
+		.with(undefined, () => undefined)
+		.otherwise((v) => {
+			try {
+				const proc = spawn("tail", ["-f", v]);
+				proc.stdout.pipe(process.stdout);
+				proc.stderr.pipe(process.stderr);
+				return proc;
+			} catch (e) {
+				console.error(`Failed to tail the log file: ${e}`);
+				return undefined;
+			}
+		});
 
 	return new Promise<void>((resolve, reject) => {
 		proc.on("exit", (code) => {
 			console.log("Server process exited with code: ", code);
+
+			if (log) log.kill();
+
 			if (code === 0) {
 				resolve();
 			} else {
