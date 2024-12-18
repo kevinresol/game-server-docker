30a94d651e9858e0390f71a4a185fb9511f4f7b1
Kevin Leung <kevinresol@gmail.com>
2024-12-18 17:05:04 +1100

    chore: logging

--- src/command/get-steam-build-id/handler.ts
+++ src/command/get-steam-build-id/handler.ts
@@ -1,4 +1,5 @@
 import { HandlerInput } from "@why-ts/cli";
+import { Logger } from "@why-ts/cli/src/lib/config/logger";
 import { delay } from "@why-ts/core";
 import { match } from "ts-pattern";
 import { z } from "zod";
@@ -13,11 +14,14 @@ export default async function ({
 	logger,
 }: HandlerInput<Args>) {
 	logger.error(`Getting Steam build ID for app=${appId}, branch=${branch}...`);
-	const data = await getSteamBuildId({ appId, branch });
+	const data = await getSteamBuildId({ appId, branch }, logger);
 	logger.log(JSON.stringify(data));
 }
 
-export async function getSteamBuildId({ appId, branch }: Args) {
+export async function getSteamBuildId(
+	{ appId, branch }: Args,
+	logger: HandlerInput<Args>["logger"]
+): Promise<{ buildId: string; timeUpdated: Date }> {
 	const MAX_ATTEMPTS = 10;
 	async function query(
 		attempt = 1
@@ -32,7 +36,11 @@ export async function getSteamBuildId({ appId, branch }: Args) {
 
 		return match(makeSteamCmdSchema(appId).parse(await res.json()))
 			.with({ status: "failed" }, async () => {
-				await delay(Math.min(120, 2 ** attempt) * 1000);
+				const wait = Math.min(120, 2 ** attempt) * 1000;
+				logger.error(
+					`Failed to query SteamCmd API. Retrying in ${wait / 1000} seconds...`
+				);
+				await delay(wait);
 				return query(attempt + 1);
 			})
 			.with({ status: "success" }, ({ data }) => {
