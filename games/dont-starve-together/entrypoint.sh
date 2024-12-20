4fa56d176c0cc5a399ebe02b76d6085c0e656002
Kevin Leung <kevinresol@gmail.com>
2024-12-20 21:21:18 +1100

    chore: update dst entrypoint

--- games/dont-starve-together/entrypoint.sh
+++ games/dont-starve-together/entrypoint.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # modify server config file
-node /home/steam/tool.js config --env-var-prefix=DST_
+# node /home/steam/tool.js config --env-var-prefix=DST_
 
 # start the server
-"$GAME_BIN_PATH/bin/dontstarve_dedicated_server_nullrenderer" -persistent_storage_root "$GAME_DATA_PATH" "$@"
+node ~/tool.js run "$@"
