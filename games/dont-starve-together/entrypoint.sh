484fd7135ab621938109a3f69dd027ead702f43e
Kevin Leung <kevinresol@gmail.com>
2024-12-21 16:37:47 +1100

    feat: forward SIGTERM from bash

--- games/dont-starve-together/entrypoint.sh
+++ games/dont-starve-together/entrypoint.sh
@@ -6,4 +6,11 @@ set -ex
 # node /home/steam/tool.js config --env-var-prefix=DST_
 
 # start the server
-node ~/tool.js run "$@"
+node ~/tool.js run "$@" &
+NODE_PID=$!
+
+# Trap SIGTERM signal
+trap 'kill -s TERM $NODE_PID' TERM
+
+# Wait for signals
+wait $NODE_PID
--- games/stationeers/entrypoint.sh
+++ games/stationeers/entrypoint.sh
@@ -5,4 +5,11 @@ set -ex
 # tail -f log.txt &
 
 # start the server
-node ~/tool.js run -- "$@"
+node ~/tool.js run -- "$@" &
+NODE_PID=$!
+
+# Trap SIGTERM signal
+trap 'kill -s TERM $NODE_PID' TERM
+
+# Wait for signals
+wait $NODE_PID
